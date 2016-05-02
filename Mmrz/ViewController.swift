819ddd60cab6f727e1a1dc4e47efdbace068529f
Lane <zhanglintc623@gmail.com>
2016-05-03 00:05:01 +0800

    label? func?

--- Mmrz/ViewController.swift
+++ Mmrz/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let label = UILabel(frame: CGRect( x: 50, y: 200, width: 100, height: 50))
+        label.textAlignment = .Center
+        label.text = "zhanglin"
+        label.center = CGPointMake(UIScreen.mainScreen().bounds.size.width/2, 200)
+        self.view.addSubview(label)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startGame(){
+        let myalert = UIAlertView()
+        myalert.title = "准备好了吗"
+        myalert.message = "准备好开始了吗？"
+        myalert.addButtonWithTitle("Ready, go!")
+        myalert.show()
+    }
 
 }
 
